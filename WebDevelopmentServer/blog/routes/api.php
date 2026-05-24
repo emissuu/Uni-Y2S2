5a175dd40fc2a4b52975b1e44c9c017e4f49dd65
Emisu <bogdan.lutsiuk@gmail.com>
2026-05-24 20:54:49 +0000

    feat: add blogposts api controller

--- WebDevelopmentServer/blog/routes/api.php
+++ WebDevelopmentServer/blog/routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group([ 'namespace' => 'App\Http\Controllers\Api\Blog', 'prefix' => 'blog'], function () {
+    Route::apiResource('posts', PostController::class)->names('blog.posts');
+});
+
