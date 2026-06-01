eaa7f972a1d9adcc2b03ec1907905cf7f40d2920
Emisu <bogdan.lutsiuk@gmail.com>
2026-06-01 11:49:03 +0000

    feat: Lab 8 | add PostController to show blog posts

--- WebDevelopmentServer/blog/routes/api.php
+++ WebDevelopmentServer/blog/routes/api.php
@@ -24,10 +24,15 @@ Route::group($groupData, function () {
     Route::apiResource('categories', CategoryController::class)
         ->only($methods)
         ->names('blog.admin.categories');
+
+    //BlogPost
+    Route::apiResource('posts', PostController::class)
+        ->except(['show'])                               //не робити маршрут для метода show
+        ->names('blog.admin.posts');
 });
 
+//Digging dipper controller
 Route::group(['prefix' => 'digging_deeper'], function () {
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
         ->name('digging_deeper.collections');
 });
-
